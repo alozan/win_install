399adf9bae4f9c1070b764e01f3c6d0c86b7fbbf
Angel Eduardo Lozano Niño <alozan@users.noreply.github.com>
2022-11-02 10:13:26 -0700

    Add files via upload

---
+++ inst.bat
@@ -0,0 +1,6 @@
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Anaconda.Anaconda3
+winget install -e --id SumatraPDF.SumatraPDF
+winget install -e --id GitHub.GitHubDesktop
+winget install -e --id Microsoft.PowerShell
+winget install -e --id Python.Python.3.9
