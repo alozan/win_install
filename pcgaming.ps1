61b8afe969f72ea823c85e8ab6d1de745a510c54
Angel Eduardo Lozano Niño <alozan@users.noreply.github.com>
2022-11-02 12:33:55 -0600

    x

--- pcgaming.ps1
+++ pcgaming.ps1
@@ -10,4 +10,9 @@ winget install -e --id EpicGames.EpicGamesLauncher
 #GOG
 winget install -e --id GOG.Galaxy
 
-#
+#Ubisoft Connect
+winget install -e --id Ubisoft.Connect
+
+#Brave
+winget install -e --id BraveSoftware.BraveBrowser
+
